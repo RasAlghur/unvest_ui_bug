d4b109bfca5c19e51345314bd31e259094e8ba62
RasAlghur <bakaretoyosi788@gmail.com>
2024-05-26 12:05:53 +0100

    n

---
+++ src/components/Constant.tsx
@@ -0,0 +1,30 @@
+export const tokenCA = "0x4FC5Cc00440F89c15e7790b1F00DC4447c7DE06D";
+
+export const contractABI = [
+    {
+        "inputs": [],
+        "name": "claim",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "account",
+                "type": "address"
+            }
+        ],
+        "name": "getClaimedAmount",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    }
+];
